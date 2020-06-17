fd85defa6059f93078c2b8ac66fa765e12158c98
Alex Hedley <alex.hedley@me.com>
2020-06-17 16:27:16 +0100

    Added RU3 build

--- build.bat
+++ build.bat
@@ -16,6 +16,7 @@ set apw=Altiris.PatchManagementCore.Web
 set adn=Altiris.DotNetLib
 set airm=Altiris.InventoryRuleManagement
 set asm=Altiris.SoftwareManagement
+set arh=Altiris.NS.Replication.Hierarchy
 
 set no_obs=find /v /i "is obsolete"
 set no_pol=find /v /i "need to supply runtime policy"
@@ -27,6 +28,40 @@ if "%1"=="7.6" goto build-7.6
 if "%1"=="8.0" goto build-8.0
 if "%1"=="8.1" goto build-8.1
 if "%1"=="8.5" goto build-8.5
+if "%1"=="8.5.3" goto build-8.5-RU3
+
+:build-8.5-RU3
+
+set ver1=v4.0_8.5.5032.0__d516cb311cfb6e4f
+set atrscm=%acm%\%ver1%\%acm%
+set atrsns=%ans%\%ver1%\%ans%
+set atrsrx=%ars%\%ver1%\%ars%
+set atrssi=%asi%\%ver1%\%asi%
+set atrsrh=%arh%\%ver1%\%arh%
+
+set ver2=v4.0_8.5.5030.0__99b1e4cc0d03f223
+set atrstm=%ats%\%ver2%\%ats%
+set atrstc=%atc%\%ver2%\%atc%
+set atrsdn=%adn%\%ver2%\%adn%
+
+set ver3=v4.0_8.5.5009.0__d516cb311cfb6e4f
+set atrspm=%apm%\%ver3%\%apm%
+
+set ver4=v4.0_8.5.3008.0__d516cb311cfb6e4f
+set invrm=%airm%\%ver4%\%airm%
+
+set ver5=v4.0_8.5.5008.0__d516cb311cfb6e4f
+set softm=%asm%\%ver5%\%asm%
+
+set fullref=/reference:%gac%\%softm%.dll /reference:%gac%\%invrm%.dll /reference:%gac%\%atrscm%.dll /reference:%gac%\%atrsns%.dll /reference:%gac%\%atrsrx%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /reference:%gac%\%atrssi%.dll /reference:%gac%\%atrstm%.dll /reference:%gac%\%atrspm%.dll /reference:%gac%\%atrstc%.dll /reference:%gac%\%atrsdn%.dll /reference:%gac%\%atrsrh%.dll
+
+set id=8.5-RU3
+
+cmd /c %csc% %fullref% /out:ZeroDayPatch-%id%.exe /win32icon:band_aid_Protirus.ico ZeroDayPatch.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs AssemblyInfo.cs | %no_obs% | %no_pol% | %no_prv%
+cmd /c %csc% %fullref% /out:PatchAutomation-%id%.exe /win32icon:band_aid_Protirus.ico PatchAutomation.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs AssemblyInfo.cs | %no_obs% | %no_pol% | %no_prv%
+cmd /c %csc% %fullref% /out:PatchExclusion-%id%.exe /win32icon:band_aid_Protirus.ico patchexclusion.cs APIWrapper.cs Constant.cs AssemblyInfo.cs | %no_obs% | %no_pol% | %no_prv%
+
+goto end
 
 :build-8.5
 
@@ -78,10 +113,10 @@ set ver3=v4.0_8.1.4538.0__d516cb311cfb6e4f
 set atrspm=%apm%\%ver3%\%apm%
 
 set ver4=v4.0_8.1.4502.0__d516cb311cfb6e4f
-set  invrm=%airm%\%ver4%\%airm%
+set invrm=%airm%\%ver4%\%airm%
 
 set ver5=v4.0_8.1.4508.0__d516cb311cfb6e4f
-set  softm=%asm%\%ver5%\%asm%
+set softm=%asm%\%ver5%\%asm%
 
 set fullref=/reference:%gac%\%softm%.dll /reference:%gac%\%invrm%.dll /reference:%gac%\%atrscm%.dll /reference:%gac%\%atrsns%.dll /reference:%gac%\%atrsrx%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /reference:%gac%\%atrssi%.dll /reference:%gac%\%atrstm%.dll /reference:%gac%\%atrspm%.dll /reference:%gac%\%atrstc%.dll /reference:%gac%\%atrsdn%.dll
 
@@ -90,6 +125,40 @@ set id=8.1
 cmd /c %csc% %fullref% /out:ZeroDayPatch-%id%.exe ZeroDayPatch.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
 cmd /c %csc% %fullref% /out:PatchAutomation-%id%.exe PatchAutomation.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
 cmd /c %csc% %fullref% /out:PatchExclusion-%id%.exe patchexclusion.cs APIWrapper.cs Constant.cs | %no_obs% | %no_pol% | %no_prv%
+
+goto end
+
+:build-8.0
+
+set ver1=v4.0_8.0.2240.0__d516cb311cfb6e4f
+set ver2=v4.0_8.0.2221.0__99b1e4cc0d03f223
+set ver3=v4.0_8.0.2217.0__d516cb311cfb6e4f
+set ver4=v4.0_8.0.2202.0__d516cb311cfb6e4f
+set ver5=v4.0_8.0.2215.0__d516cb311cfb6e4f
+
+set invrm=%airm%\%ver4%\%airm%
+set softm=%asm%\%ver5%\%asm%
+set atrscm=%acm%\%ver1%\%acm%
+set atrsns=%ans%\%ver1%\%ans%
+set atrsrx=%ars%\%ver1%\%ars%
+set atrstm=%ats%\%ver2%\%ats%
+set atrstc=%atc%\%ver2%\%atc%
+set atrsdn=%adn%\%ver2%\%adn%
+set atrspm=%apm%\%ver3%\%apm%
+
+set fullref=/reference:%gac%\%softm%.dll /reference:%gac%\%invrm%.dll /reference:%gac%\%atrscm%.dll /reference:%gac%\%atrsns%.dll /reference:%gac%\%atrsrx%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /reference:%gac%\%asi%\%ver1%\%asi%.dll /reference:%gac%\%atrstm%.dll /reference:%gac%\%atrspm%.dll /reference:%gac%\%atrstc%.dll /reference:%gac%\%atrsdn%.dll
+
+set id=8.0
+
+cmd /c %csc% %fullref% /out:ZeroDayPatch-%id%.exe ZeroDayPatch.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
+
+cmd /c %csc% %fullref% /out:PatchAutomation-%id%.exe PatchAutomation.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
+
+cmd /c %csc% %fullref% /out:PatchExclusion-%id%.exe patchexclusion.cs APIWrapper.cs Constant.cs | %no_obs% | %no_pol% | %no_prv%
+
+cmd /c %csc% %fullref% /out:EnableBulletinByName-%id%.exe EnableBulletinByName.cs APIWrapper.cs
+
+cmd /c %csc% %fullref% /out:GetDisabledBulletins-%id%.exe GetDisabledBulletins.cs
 
 goto end
 
@@ -108,6 +177,7 @@ cmd /c %csc% /reference:%gac%\%asm%\%ver1%\%asm%.dll /reference:%gac%\%airm%\%ve
 goto end
 
 :build-7.5
+
 set ver1=7.5.3153.0__d516cb311cfb6e4f
 set ver2=7.5.3219.0__d516cb311cfb6e4f
 set ver3=7.5.3153.0__99b1e4cc0d03f223
@@ -123,6 +193,7 @@ cmd /c %csc% /reference:%gac%\%asm%\%ver1%\%asm%.dll /reference:%gac%\%airm%\%ve
 goto end
 
 :build-7.1
+
 set ver1=7.1.8400.0__d516cb311cfb6e4f
 set ver2=7.1.7858.0__d516cb311cfb6e4f
 set ver3=7.1.8400.0__99b1e4cc0d03f223
@@ -136,39 +207,6 @@ cmd /c %csc% /reference:%gac%\%asm%\%ver1%\%asm%.dll /reference:%gac%\%airm%\%ve
 
 cmd /c %csc% /reference:%gac%\%asm%\%ver1%\%asm%.dll /reference:%gac%\%airm%\%ver1%\%airm%.dll /reference:%gac%\%acm%\%ver1%\%acm%.dll /reference:%gac%\%ans%\%ver1%\%ans%.dll /reference:%gac%\%ars%\%ver1%\%ars%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /reference:%gac%\%asi%\%ver1%\%asi%.dll /reference:%gac%\%ats%\%ver3%\%ats%.dll /reference:%gac%\%apm%\%ver2%\%apm%.dll /reference:%gac%\%atc%\%ver3%\%atc%.dll /reference:%gac%\%adn%\%ver3%\%adn%.dll /out:PatchExclusion-7.1.exe patchexclusion.cs APIWrapper-7.1.cs Constant.cs
 
-
-:build-8.0
-
-set ver1=v4.0_8.0.2240.0__d516cb311cfb6e4f
-set ver2=v4.0_8.0.2221.0__99b1e4cc0d03f223
-set ver3=v4.0_8.0.2217.0__d516cb311cfb6e4f
-set ver4=v4.0_8.0.2202.0__d516cb311cfb6e4f
-set ver5=v4.0_8.0.2215.0__d516cb311cfb6e4f
-
-set invrm=%airm%\%ver4%\%airm%
-set  softm=%asm%\%ver5%\%asm%
-set atrscm=%acm%\%ver1%\%acm%
-set atrsns=%ans%\%ver1%\%ans%
-set atrsrx=%ars%\%ver1%\%ars%
-set atrstm=%ats%\%ver2%\%ats%
-set atrstc=%atc%\%ver2%\%atc%
-set atrsdn=%adn%\%ver2%\%adn%
-set atrspm=%apm%\%ver3%\%apm%
-
-set fullref=/reference:%gac%\%softm%.dll /reference:%gac%\%invrm%.dll /reference:%gac%\%atrscm%.dll /reference:%gac%\%atrsns%.dll /reference:%gac%\%atrsrx%.dll /reference:%gac%\%adb%\%ver1%\%adb%.dll /reference:%gac%\%asi%\%ver1%\%asi%.dll /reference:%gac%\%atrstm%.dll /reference:%gac%\%atrspm%.dll /reference:%gac%\%atrstc%.dll /reference:%gac%\%atrsdn%.dll
-
-
-set id=8.0
-
-cmd /c %csc% %fullref% /out:ZeroDayPatch-%id%.exe ZeroDayPatch.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
-
-cmd /c %csc% %fullref% /out:PatchAutomation-%id%.exe PatchAutomation.cs Constant.cs APIWrapper.cs CLIConfig.cs CLIInit.cs | %no_obs% | %no_pol% | %no_prv%
-
-cmd /c %csc% %fullref% /out:PatchExclusion-%id%.exe patchexclusion.cs APIWrapper.cs Constant.cs | %no_obs% | %no_pol% | %no_prv%
-
-cmd /c %csc% %fullref% /out:EnableBulletinByName-%id%.exe EnableBulletinByName.cs APIWrapper.cs
-
-cmd /c %csc% %fullref% /out:GetDisabledBulletins-%id%.exe GetDisabledBulletins.cs
 goto end
 
 :end
